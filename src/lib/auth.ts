e33cbb93d330b989b83c6444b09a9fc6ccacf9d8
Refat al hasan <mdrefatalhasan@protonmail.com>
2026-02-02 18:48:42 +0600

    struggling with frontend

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,8 +50,9 @@ export const auth = betterAuth({
       },
       address: {
         type: "string",
-        required: true,
+        required: false,
         input: true,
+
       },
     },
   },
