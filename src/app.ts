e3047ce7587ccade853b86619f4f9dc49d91337e
Refat al hasan <236903239+refatalhasankaif@users.noreply.github.com>
2026-02-05 15:51:40 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ app.use(cors({
     origin: [
         "http://localhost:3000",
         "http://localhost:5000",
+        "https://foodhub-backend-3poi.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
