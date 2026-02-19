5e0163ad2fd2b5444f4e217a995a865cd0ab6019
Refat al hasan <refatalhasank@protonmail.com>
2026-02-19 21:06:49 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,12 @@ import { reviewRoutes } from "./module/reviews/reviews.routes";
 import { adminRoutes } from "./module/admin/admin.routes";
 import { notFound } from "./middlewares/notFound";
 import { categoryRoutes } from "./module/categories/category.routes";
-import { env } from "process";
 
 const app: Application = express();
 
 app.use(
   cors({
-    origin: env.ORIGIN_URL || "http://localhost:3000",
+    origin: process.env.ORIGIN_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
     exposedHeaders: ["set-cookie"],
